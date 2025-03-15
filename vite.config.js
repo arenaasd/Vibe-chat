ccce25533cb9242ed122a151f154d5fb0b846089
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-16 04:17:02 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://chat-app-backend-roan-three.vercel.app',  // Your backend URL
+        changeOrigin: true,  // Ensures the Host header matches the backend
+        secure: true,  // Use this if your backend uses HTTPS (recommended)
+        rewrite: (path) => path.replace(/^\/api/, ''),  // Optional: rewrite '/api' prefix if needed
+      },
+    },
+  },
 });
